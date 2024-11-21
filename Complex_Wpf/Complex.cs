7010e2889d5c000bf0b69653e625bec922093551
Kemran <morpeh2005ka@gmail.com>
2024-11-21 19:38:49 +0300

    Сделал Gui

---
+++ Complex_Wpf/Complex.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Complex_Wpf
+{
+    /// <summary>
+    /// Class for complex numbers
+    /// </summary>
+    public struct Complex
+    {
+        public double a;
+        public double b;
+        private double abs;
+        private double fi;
+        /// <summary>
+        /// Конструктор для Complex
+        /// </summary>
+        /// <param name="a">вещественная часть</param>
+        /// <param name="b">Мнимая часть</param>
+        public Complex(double a = 0, double b = 0)
+        {
+            this.a = a; this.b = b;
+            abs = Math.Sqrt(a*a + b*b);
+            fi = Math.Atan2(b, a);
+        }  
+        /// <summary>
+        /// getter for abs
+        /// </summary>
+        public double Abs
+        {
+            get { return abs; }
+            set { throw new ArgumentException("Cant change abs of complex number, use AbsCalculate"); }
+        }
+        /// <summary>
+        /// Convert Complex to String
+        /// </summary>
+        /// <returns>String</returns>
+        public override string ToString() => $"{this.a}{(this.b >= 0 ? "+" : "")}{this.b}i";
+        /// <summary>
+        /// Перевод в экспоненту
+        /// </summary>
+        /// <param name="a">Число а</param>
+        /// <returns></returns>
+        public static (double, double) ToExp(Complex a) => (a.abs, a.fi);
+        /// <summary>
+        /// Перевод в нормальное число
+        /// </summary>
+        /// <param name="abs">Модуль числа </param>
+        /// <param name="fi">Угол фи</param>
+        /// <returns>Возвращает комплексное число в обычной записи</returns>
+        public static Complex FromExp(double abs, double fi) => new Complex(abs * Math.Cos(fi), abs * Math.Sin(fi));
+        /// <summary>
+        /// Sum of Complex numbers
+        /// </summary>
+        /// <param name="a">First complex number</param>
+        /// <param name="b">Second complex number</param>
+        /// <returns>Sum of complex numbers</returns>
+        public static Complex operator +(Complex a, Complex b) => new Complex(a.a+b.a, a.b+b.b);
+        /// <summary>
+        /// Sub of Complex numbers
+        /// </summary>
+        /// <param name="a">First complex number</param>
+        /// <param name="b">Second complex number</param>
+        /// <returns>Sub of complex numbers</returns>
+        public static Complex operator -(Complex a, Complex b) => new Complex(a.a - b.a, a.b - b.b);
+        /// <summary>
+        /// Multiply of Complex numbers
+        /// </summary>
+        /// <param name="a">First complex number</param>
+        /// <param name="b">Second complex number</param>
+        /// <returns>Multiply of complex numbers</returns>
+        public static Complex operator *(Complex a, Complex b) => new Complex(a.a*b.a-a.b*b.b, a.b*b.a+a.a*b.b);
+        /// <summary>
+        /// Division of Complex numbers
+        /// </summary>
+        /// <param name="a">First complex number</param>
+        /// <param name="b">Second complex number</param>
+        /// <returns>Division of complex numbers</returns>
+        public static Complex operator /(Complex a, Complex b) => new Complex((a.a * b.a + a.b * b.b)/(b.a*b.a+b.b*b.b), (a.b * b.a - a.a * b.b)/(b.a * b.a + b.b * b.b));
+        /// <summary>
+        /// Operator of ==
+        /// </summary>
+        /// <param name="a">First complex number</param>
+        /// <param name="b">Second complex number</param>
+        /// <returns></returns>
+        public static bool operator ==(Complex a, Complex b) => a.a == b.a && a.b == b.b;
+        /// <summary>
+        /// Operator of !=
+        /// </summary>
+        /// <param name="a">First complex number</param>
+        /// <param name="b">Second complex number</param>
+        /// <returns></returns>
+        public static bool operator !=(Complex a, Complex b) => !(a==b); 
+        /// <summary>
+        /// Is equal?
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>true pr false</returns>
+        public override bool Equals(object? obj)
+        {
+            if (obj is Complex other)
+            {
+                return this == other;
+            }
+            return false;
+        }
+        /// <summary>
+        /// GetHashCodeFunction
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode() => (a, b).GetHashCode();
+    }
+}
---
+++ Complex_Wpf/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Complex_Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// First Complex Number
+        /// </summary>
+        private Complex compl1;
+        /// <summary>
+        /// Second Complex Number
+        /// </summary>
+        private Complex compl2;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// TxtBox checker
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // преобразовываем объект посылающий событие в TextBox
+            TextBox? txt = sender as TextBox;
+            // если преобразование успешно
+            if (txt != null)
+            {
+                // проверяем можно ли спарсить введенный текст
+                e.Handled = !double.TryParse(e.Text + txt.Text, out _);
+            }
+        }
+        /// <summary>
+        /// When Button Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            compl1 = new Complex(double.Parse(Rel1.Text), double.Parse(Img1.Text));
+            compl2 = new Complex(double.Parse(Rel2.Text), double.Parse(Img2.Text));
+            Exp1.Text = FromExpToString(compl1);
+            Exp2.Text = FromExpToString(compl2);
+            Sum.Text = (compl1 + compl2).ToString();
+            Sub.Text = (compl1 - compl2).ToString();
+            Mul.Text = (compl1 * compl2).ToString();
+            Div.Text = (compl1 / compl2).ToString();
+            SumE.Text = FromExpToString(compl1 + compl2);
+            SubE.Text = FromExpToString(compl1 - compl2);
+            MulE.Text = FromExpToString(compl1 * compl2);
+            DivE.Text = FromExpToString(compl1 / compl2);
+        }
+        /// <summary>
+        /// From exp to string
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <returns>String</returns>
+        private string FromExpToString(Complex exp)
+        {
+            var obg1 = Complex.ToExp(exp);
+            return $"{obg1.Item1}*e^(i{obg1.Item2})";
+        }
+    }
+}
